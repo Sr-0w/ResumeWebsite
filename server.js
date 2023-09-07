4e28e17e707357d93e5804211813ae2e9b30c84f
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-09-07 20:25:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const PORT = 3000;
+const https = require('https');
+const fs = require('fs');
 
-// Servir les fichiers statiques
+const app = express();
+const PORT = 443;
+
+// Middleware to log all requests
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl} - ${req.ip}`);
+    next();
+});
+
+// Serve static files
 app.use(express.static(__dirname));
 
-// Route pour servir le fichier PDF
+// Route to serve the PDF file
 app.get('/download-resume', (req, res) => {
     const filePath = path.join(__dirname, 'resume.pdf');
+    console.log(`${new Date().toISOString()} - PDF download by ${req.ip}`);
     res.download(filePath, 'Robin Snyders Resume.pdf');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const httpsOptions = {
+    cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+    key: fs.readFileSync(path.join(__dirname, 'key.pem'))
+};
+
+const server = https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log(`Server is running on https://localhost:${PORT}`);
 });
