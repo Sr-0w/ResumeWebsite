d7d94298b911e76c922ab545e9af51b9f13b5123
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2023-09-21 14:24:58 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,9 +2,36 @@ const express = require('express');
 const path = require('path');
 const https = require('https');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
+const stack = require('stack');
 
 const app = express();
-const PORT = 443;
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
+
+// Middleware for redirecting HTTP traffic to HTTPS
+const httpApp = express();
+httpApp.all('*', (req, res) => {
+    res.redirect(301, `https://${req.headers.host}${req.url}`);
+});
+
+httpApp.listen(HTTP_PORT, () => {
+    console.log(`Redirecting all HTTP traffic to HTTPS on port ${HTTP_PORT}`);
+});
+
+// Set up Nodemailer for Microsoft 365
+let transporter = nodemailer.createTransport({
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false,  // false for TLS
+    auth: {
+        user: 'robin@snyders.xyz',
+        pass: 'Sth_7849807'  // Use environment variable for password
+    },
+    tls: {
+        ciphers: 'SSLv3'
+    }
+});
 
 // Middleware to log all requests
 app.use((req, res, next) => {
@@ -15,10 +42,27 @@ app.use((req, res, next) => {
 // Serve static files
 app.use(express.static(__dirname));
 
-// Route to serve the PDF file
+// Route to serve the PDF file and send email notification
 app.get('/download-resume', (req, res) => {
     const filePath = path.join(__dirname, 'resume.pdf');
     console.log(`${new Date().toISOString()} - PDF download by ${req.ip}`);
+
+    // Send email notification
+    let mailOptions = {
+        from: 'robin@snyders.xyz',
+        to: 'robin@snyders.xyz',
+        subject: 'Resume Download Alert',
+        text: `Someone with IP ${req.ip} downloaded the resume.`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
+
     res.download(filePath, 'Robin Snyders Resume.pdf');
 });
 
@@ -27,6 +71,6 @@ const httpsOptions = {
     key: fs.readFileSync(path.join(__dirname, 'key.pem'))
 };
 
-const server = https.createServer(httpsOptions, app).listen(PORT, () => {
-    console.log(`Server is running on https://localhost:${PORT}`);
+const server = https.createServer(httpsOptions, app).listen(HTTPS_PORT, () => {
+    console.log(`Server is running on https://localhost:${HTTPS_PORT}`);
 });
