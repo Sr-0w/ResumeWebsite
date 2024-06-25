4e36d316348718821976ae446a15cdc97cb167ee
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2024-06-25 23:08:41 +0200

    s

--- scroll-animation.js
+++ scroll-animation.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     }, {
-        rootMargin: '-300px',
+        rootMargin: '-100px',
         threshold: 0.1
     });
 
