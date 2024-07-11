539256d4c62378283c60c4e2412f856845854973
Sr-0w <139899120+Sr-0w@users.noreply.github.com>
2024-07-11 22:27:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
     ];
 
     // Allow static assets
-    const staticFileExtensions = ['.css', '.webp', '.js', '.png', '.jpg', '.jpeg', '.gif', '.svg', '.woff', '.woff2', '.ttf', '.otf', '.eot', '.pdf'];
+    const staticFileExtensions = ['.css', '.webp', '.js', '.png', '.jpg', '.jpeg', '.gif', '.svg', '.woff', '.woff2', '.ttf', '.otf', '.eot', '.ico', '.pdf'];
     const isStaticAsset = staticFileExtensions.some(ext => req.path.endsWith(ext));
 
     if (!allowedPaths.includes(req.path) && !isStaticAsset) {
